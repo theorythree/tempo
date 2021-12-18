305a25d53932c69fae24f06e0c0e8202f6975f31
Dan Merfeld <dan@theorythree.com>
2021-12-18 13:03:03 -0600

    adds project resource routes to web routes

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// TODO: replace the client routes with a single resource route
 Route::post('/clients','App\Http\Controllers\ClientController@store');
 Route::put('/clients/{client}','App\Http\Controllers\ClientController@update');
 Route::delete('/clients/{client}','App\Http\Controllers\ClientController@destroy');
 Route::get('/clients/{client}','App\Http\Controllers\ClientController@show');
 Route::get('/clients','App\Http\Controllers\ClientController@index');
+
+Route::resource('/projects','App\Http\Controllers\ProjectController');
